db31b38d2d9e3c22ccb4b6e02024205f40b34161
laura-feher <lfeher@nps.gov>
2024-06-06 09:42:51 -0400

    :art: Renamed to 'example_data' since marker horizon data was added.

--- data-raw/example_data.R
+++ data-raw/example_data.R
@@ -23,6 +23,11 @@ example_sets <- dbGetQuery(con, 'SELECT * FROM ssrs.vw_dbx_SET_data_FINAL') %>%
     select(event_date_UTC, network_code, park_code, site_name, station_code, SET_direction, pin_position, pin_height_mm) %>%
     filter(park_code == "ASIS" & site_name == "Marsh 11")
 
-usethis::use_data(example_sets, overwrite = T)
+example_mh <- dbGetQuery(con, 'SELECT * FROM ssrs.vw_dbx_marker_horizon_data') %>%
+    filter(network_code == "NCBN") %>%
+    select(event_date_UTC, network_code, park_code, site_name, station_code, marker_horizon_name, core_measurement_number, core_measurement_depth_mm, established_date) %>%
+    filter(park_code == "ASIS" & site_name == "Marsh 11")
+
+usethis::use_data(example_sets, example_mh, overwrite = T)
 
 DBI::dbDisconnect(con)
