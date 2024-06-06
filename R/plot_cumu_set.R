ff298b9989939fcc56279332fa2a18ae27224222
laura-feher <lfeher@nps.gov>
2024-06-06 08:44:33 -0400

    :art: Moved the functionality of plot_cumu_arm and plot_cumu_set into a single function. Added labels with linear rates of change to station-level plots.

--- R/plot_cumu_set.R
+++ R/plot_cumu_set.R
@@ -1,74 +1,239 @@
-#' Make a graph of change over time by SET (or station)
+#' Plot SET cumulative change
 #'
-#' x-axis is date; y-axis is the average of the 36 pin heights' difference from baseline (first measurement). One facet per SET/station id.
+#' @param data A data frame of SET data. See details below for requirements.
 #'
-#' @param data Data frame with one row per pin reading, and the following columns, named exactly: event_date_UTC, network_code, park_code, site_name, station_code, SET_direction, pin_position, and pin_height_mm.
+#' @param data_level Level of SET data to plot. One of "arm" or "station". "arm"
+#'   shows arm-level cumulative change at 1 or more stations. "station" shows
+#'   station-level interval change by station_code at 1 or more stations.
 #'
-#' @param columns Number of columns you want in the faceted output; defaults to 4.
+#' @param station (optional) Plot SET cumulative data from specific station(s).
+#'   Refers to the 'station_code' column in the SET data. Can be a single
+#'   station code (e.g. "M11-1") or vector of multiple stations (e.g. c("M11-1",
+#'   "M11-2)).
 #'
-#' @param pointsize Size of points you want (goes into the `size` argument of `ggplot2::geom_point`); defaults to 3.5.
+#' @param site (optional) Plot SET cumulative data from specific site(s). Refers
+#'   to the 'site_name' column in the SET data. Can be a single site name (e.g.
+#'   "Marsh 11") or vector of multiple sites (e.g. c("Marsh 11", "Marsh 3")).
 #'
-#' @param scales Do you want axis scales to be the same in all facets ("fixed") or to vary between facets "free_x" or "free_y" or "free" - goes into `scales` arg of `facet_wrap`; defaults to "free_y".
+#' @param park (optional) Plot SET cumulative data from specific park(s). Refers
+#'   to the 'park_code' column in the SET data. Can be a single park code (e.g.
+#'   "ASIS") or vector of multiple parks (e.g. c("ASIS", "GATE")) although it is
+#'   not recommended to plot multiple parks because the individual station-level
+#'   panels will be hard to see.
 #'
-#' @param smooth Do you want a linear regression plotted on top? defaults to TRUE.
+#' @param columns Number of columns you want in the faceted output; defaults to
+#'   4.
 #'
-#' @param lty_smooth What type of line do you want the linear regression to plot as? 1 = solid; 2 and 5 = dashed; or any of the other line types available in `ggplot2`. defaults to dashed.
+#' @param pointsize Size of points you want (goes into the `size` argument of
+#'   `ggplot2::geom_point`); defaults to 3.5.
 #'
-#' @return a ggplot object
+#' @param scales Do you want axis scales to be the same in all facets ("fixed")
+#'   or to vary between facets "free_x" or "free_y" or "free" - goes into
+#'   `scales` arg of `facet_wrap`; defaults to "free_y".
+#'
+#' @description Arm- and station-level cumulative change is calculated via the function
+#'   'calc_change_cumu'. Linear rates of change are calculated via the function
+#'   'calc_linear_rates'. See function documentation for details.
+#'
+#' @details `data` must be data frame of SET data with 1 row per pin reading and
+#'   the following columns, named exactly: event_date_UTC, network_code,
+#'   park_code, site_name, station_code, SET_direction, pin_position, and
+#'   pin_height_mm.
+#'
+#' @return a ggplot object: x-axis is date; y-axis is the average of the 36 pin
+#'   heights' difference from baseline (first measurement). One facet per
+#'   station.
 #'
 #' @export
 #'
 #' @examples
-#' plot_cumu_set(example_sets)
-#' plot_cumu_set(example_sets, columns = 1, pointsize = 2, smooth = FALSE)
+#' plot_cumu_set(example_sets, data_level = "arm", site = "Elders East", columns = 3, pointsize = 2)
+#' plot_cumu_set(example_sets, data_level = "station")
+#'
+plot_cumu_set <- function(data, data_level, station = NULL, site = NULL , park = NULL, columns = 4, pointsize = 2, scales = "fixed") {
 
-plot_cumu_set <- function(data, set = NULL, columns = 4, pointsize = 3.5, scales = "fixed", smooth = TRUE, lty_smooth = 5){
+    # determine if the data has the correct columns for SET data
+    cols <- colnames(data)
 
-    if(is.null(set)){
-        to_plot <- data
-        plot_title <- 'Cumulative Change by arm position'
-    }
-    else{
-        to_plot <- data %>%
-            dplyr::filter(station_code == !!set)
+    ## conditions: have correct columns in data frame
+    ## stop and give an informative message if this isn't met
+    req_clms <- c("event_date_UTC", "network_code", "park_code", "site_name", "station_code", "SET_direction", "pin_position", "pin_height_mm")
 
-        station_lab <- data %>%
-            filter(station_code == !!set) %>%
-            distinct(park_code, site_name, station_code) %>%
-            mutate(lab = paste(park_code, site_name, station_code, sep = ", ")) %>%
-            pull(lab)
-
-        plot_title <- paste('Cumulative Change by arm position at\n', station_lab)
+    if(sum(req_clms %in% names(data)) != length(req_clms)){
+        stop(paste("Your data frame must have the following columns, with these names, but is missing at least one:", paste(req_clms, collapse = ", ")))
     }
 
-    dataf <- calc_change_cumu(to_plot)
-    dataf <- dataf$set
+    list_cumu_data <- calc_change_cumu(data)
 
-    # calculate linear rates of change for each SET/station
-    rates <- calc_set_rates(to_plot)
+    if(data_level != "arm" & data_level != "station") {
+        stop("data_level required: please choose either 'arm' or 'station' for data_level")
+    }
 
-    # data needs to be the $set piece of the output from calc_change_cumu
-    ggplot2::ggplot(dataf, ggplot2::aes(x = event_date_UTC, y = mean_cumu)) +
-        ggplot2::geom_line(col = 'lightsteelblue4') +
-        {if(smooth) ggplot2::geom_smooth(se = FALSE, method = 'lm',
-                                col = 'steelblue4', lty = lty_smooth, size = 1)} +
-        ggplot2::geom_point(shape = 21,
-                   fill = 'lightsteelblue1', col = 'steelblue3',
-                   size = pointsize, alpha = 0.9) +
-        {if(smooth) ggplot2::geom_text(
-            data = rates,
-            aes(x = structure(Inf, class = "Date"), y = Inf, label =
-                    paste("rate:", format(round(set_rate, 2), nsmall = 2), "+",
-                          format(round(se_rate, 2), nsmall = 2), "\n" , "r2:",
-                          format(round(set_rate_r2, 2), nsmall = 2), sep = " ")),
-            hjust = 1,
-            vjust = 2)} +
-        ggplot2::facet_wrap(~station_code, ncol = columns, scales = scales) +
-        {if(smooth) ggplot2::labs(title = 'Cumulative Change since first reading',
-                         x = 'Date',
-                         y = 'Change since first reading (mm)')} +
-        {if(!smooth) ggplot2::labs(title = 'Cumulative Change since first reading',
-                          x = 'Date',
-                          y = 'Change since first reading (mm)')} +
-        ggplot2::theme_classic()
+    list_stations <- data %>%
+        distinct(station_code) %>%
+        pull()
+
+    list_sites <- data %>%
+        distinct(site_name) %>%
+        pull()
+
+    list_parks <- data %>%
+        distinct(park_code) %>%
+        pull()
+
+    if(is.null(station) & is.null(site) & is.null(park)) {
+        if(data_level == "arm"){
+
+            cumu_data <- list_cumu_data$arm %>%
+                mutate(site_station_id = paste(park_code, site_name, station_code, sep = ", "))
+
+            ggplot2::ggplot(cumu_data, ggplot2::aes(x = event_date_UTC, y = mean_cumu, col = as.factor(SET_direction))) +
+                ggplot2::geom_point(size = pointsize) +
+                ggplot2::geom_line() +
+                ggplot2::facet_wrap(~site_station_id, ncol = columns, scales = scales) +
+                ggplot2::labs(title = "Cumulative surface elevation change by SET direction",
+                              x = 'Date',
+                              y = 'Cumulative surface elevation change (mm)',
+                              color = "SET direction") +
+                ggplot2::theme_bw() +
+                ggplot2::theme(legend.position = 'bottom')
+
+        }
+        else if(data_level == "station"){
+
+            cumu_data <- list_cumu_data$station %>%
+                mutate(site_station_id = paste(park_code, site_name, station_code, sep = ", "))
+
+            rates <- calc_linear_rates(data) %>%
+                mutate(site_station_id = paste(park_code, site_name, station_code, sep = ", "))
+
+            ggplot2::ggplot(cumu_data, ggplot2::aes(x = event_date_UTC, y = mean_cumu)) +
+                ggplot2::geom_line(col = 'lightsteelblue4') +
+                ggplot2::geom_smooth(se = FALSE, method = 'lm',
+                                     col = 'steelblue4', linewidth = 1) +
+                ggplot2::geom_point(shape = 21,
+                                    fill = 'lightsteelblue1', col = 'steelblue3',
+                                    size = pointsize, alpha = 0.9) +
+                ggplot2::geom_text(
+                    data = rates,
+                    aes(x = structure(Inf, class = "Date"), y = Inf, label =
+                            paste("rate:", format(round(station_rate, 2), nsmall = 2), "+",
+                                  format(round(station_se_rate, 2), nsmall = 2), "\n" , "r2:",
+                                  format(round(station_rate_r2, 2), nsmall = 2), sep = " ")),
+                    hjust = 1,
+                    vjust = 2) +
+                ggplot2::facet_wrap(~site_station_id, ncol = columns, scales = scales) +
+                ggplot2::labs(title = 'Cumulative surface elevation change by station',
+                              x = 'Date',
+                              y = 'Cumulative surface elevation change (mm)') +
+                ggplot2::theme_classic()
+        }
+    }
+
+    else {
+
+        # if any stations/sites/parks are not found, print a warning but plot the stations/sites/parks that are found
+        # if all stations/sites/parks are not found, stop on error
+
+        unmatched_stations <- station[!station %in% list_stations]
+        matched_stations <- station[station %in% list_stations]
+        if(length(matched_stations) > 0 & length(unmatched_stations) > 0) {
+            warning(paste0(" station '", unmatched_stations, "' not found "))
+        }
+        else if(length(matched_stations) == 0 & length(unmatched_stations) > 0) {
+            stop(paste0("station '", unmatched_stations, "' not found "))
+        }
+
+        unmatched_sites <- site[!site %in% list_sites]
+        matched_sites <- site[site %in% list_sites]
+        if(length(matched_sites) > 0 & length(unmatched_sites) > 0) {
+            warning(paste0(" site '", unmatched_site, "' not found "))
+        }
+        else if(length(matched_sites) == 0 & length(unmatched_sites) > 0) {
+            stop(paste0("site '", unmatched_sites, "' not found "))
+        }
+
+        unmatched_parks <- park[!park %in% list_parks]
+        matched_parks <- park[park %in% list_parks]
+        if(length(matched_parks) > 0 & length(unmatched_parks) > 0) {
+            warning(paste0(" park '", unmatched_parks, "' not found "))
+        }
+        else if(length(matched_parks) == 0 & length(unmatched_parks) > 0) {
+            stop(paste0("park '", unmatched_parks, "' not found "))
+        }
+
+        if(data_level == "arm"){
+            cumu_data <- list_cumu_data$arm
+
+            to_plot <- cumu_data %>%
+                {
+                    if(!is.null(station))
+                        filter(., station_code %in% !!station)
+                    else if(!is.null(site))
+                        filter(., site_name %in% !!site)
+                    else if(!is.null(park))
+                        filter(., park_code %in% !!park)
+                } %>%
+                mutate(site_station_id = paste(park_code, site_name, station_code, sep = ", "))
+
+            ggplot2::ggplot(to_plot, ggplot2::aes(x = event_date_UTC, y = mean_cumu, col = as.factor(SET_direction))) +
+                ggplot2::geom_point(size = pointsize) +
+                ggplot2::geom_line() +
+                ggplot2::facet_wrap(~site_station_id, ncol = columns, scales = scales) +
+                ggplot2::labs(title = "Cumulative surface elevation change by SET direction",
+                              x = 'Date',
+                              y = 'Cumulative surface elevation change (mm)',
+                              color = "SET direction") +
+                ggplot2::theme_bw() +
+                ggplot2::theme(legend.position = 'bottom')
+        }
+
+        else if(data_level == "station"){
+            cumu_data <- list_cumu_data$station
+
+            to_plot <- cumu_data %>%
+                {
+                    if(!is.null(station))
+                        filter(., station_code %in% !!station)
+                    else if(!is.null(site))
+                        filter(., site_name %in% !!site)
+                    else if(!is.null(park))
+                        filter(., park_code %in% !!park)
+                } %>%
+                mutate(site_station_id = paste(park_code, site_name, station_code, sep = ", "))
+
+            # calculate linear rates of change for each station
+            rates <- calc_linear_rates(data) %>%
+                {
+                    if(!is.null(station))
+                        filter(., station_code %in% !!station)
+                    else if(!is.null(site))
+                        filter(., site_name %in% !!site)
+                    else if(!is.null(park))
+                        filter(., park_code %in% !!park)
+                } %>%
+                mutate(site_station_id = paste(park_code, site_name, station_code, sep = ", "))
+
+            ggplot2::ggplot(to_plot, ggplot2::aes(x = event_date_UTC, y = mean_cumu)) +
+                ggplot2::geom_line(col = 'lightsteelblue4') +
+                ggplot2::geom_smooth(se = FALSE, method = 'lm',
+                                     col = 'steelblue4', linewidth = 1) +
+                ggplot2::geom_point(shape = 21,
+                                    fill = 'lightsteelblue1', col = 'steelblue3',
+                                    size = pointsize, alpha = 0.9) +
+                ggplot2::geom_text(
+                    data = rates,
+                    aes(x = structure(Inf, class = "Date"), y = Inf, label =
+                            paste("rate:", format(round(station_rate, 2), nsmall = 2), "+",
+                                  format(round(station_se_rate, 2), nsmall = 2), "\n" , "r2:",
+                                  format(round(station_rate_r2, 2), nsmall = 2), sep = " ")),
+                    hjust = 1,
+                    vjust = 2) +
+                ggplot2::facet_wrap(~site_station_id, ncol = columns, scales = scales) +
+                ggplot2::labs(title = 'Cumulative surface elevation change by station',
+                              x = 'Date',
+                              y = 'Cumulative surface elevation change (mm)') +
+                ggplot2::theme_classic()
+        }
+    }
 }
