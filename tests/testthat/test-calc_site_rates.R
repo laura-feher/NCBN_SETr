77d13857ea8f378f906929ec16657c883362edd1
laura-feher <lfeher@nps.gov>
2025-03-03 17:11:19 -0500

    :fire: removed functions for QAQC since those are already covered in the QAQC dashboard.

--- tests/testthat/test-calc_site_rates.R
+++
@@ -1,4 +0,0 @@
-test_that("returns a nested data frame", {
-    df <- readr::read_csv(test_path("testdata", "test_list_calc_change_cumu.csv"), col_types = "Dcccccin")
-    expect_type(calc_site_rates(df), "list")
-})
