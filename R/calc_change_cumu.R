9a91747fdce5001ec42603951515c29ecce7ee08
laura-feher <lfeher@nps.gov>
2025-03-05 14:26:53 -0500

    🔥 removed pin, arm, and mh-level calculations so that everything is calculated at station-level

--- R/calc_change_cumu.R
+++ R/calc_change_cumu.R
@@ -30,95 +30,79 @@
 #'   event_date_UTC, network_code, park_code, site_name, marker_horizon_name,
 #'   core_measurement_number, core_measurement_depth_mm, and established_date.
 #'
-#' @return For SET data, returns a list of three tibbles: one each for pin-,
-#'   arm-, and station-level calculations.
-#'
-#'   For MH data, returns a list of two tibbles: one for each marker horizon-
-#'   and station-level calculations.
+#' @return For SET data, returns a data frame of station-level cumulative
+#'   surface elevation change. For MH data, returns a data frame of
+#'   station-level vertical accretion.
 #'
 #' @export
+#'
+#' @import dplyr
 #'
 #' @examples
 #' calc_change_cumu(example_sets)
 #'
+#' calc_change_cumu(example_mh)
+#'
 calc_change_cumu <- function(data) {
 
     # determine if the data is SET or MH
-    cols <- colnames(data)
+    data_type <- detect_data_type(data)
 
-    if("pin_height_mm" %in% cols){
+    ## do calculations based on data type
+    if(data_type != "SET" & data_type != "MH") {
+        stop(paste0("data must be either SET or MH data and contain the correct columns"))
 
-        ## conditions: have correct columns in data frame
-        ## stop and give an informative message if this isn't met
-        req_clms <- c("event_date_UTC", "network_code", "park_code", "site_name", "station_code", "SET_direction", "pin_position", "pin_height_mm")
+    } else if(data_type == "SET") {
 
-        if(sum(req_clms %in% names(data)) != length(req_clms)){
-            stop(paste("Your data frame must have the following columns, with these names, but is missing at least one:", paste(req_clms, collapse = ", ")))
-        }
+        change_cumu_set_station <- data %>%
 
-    ## calculations
+            # first get cumulative change for each pin
+            dplyr::arrange(network_code, park_code, site_name, station_code, SET_direction, pin_position, event_date_UTC) %>%
+            dplyr::group_by(network_code, park_code, site_name, station_code, SET_direction, pin_position) %>%
+            dplyr::mutate(first_pin_height = pin_height_mm[event_date_UTC == min(event_date_UTC[!is.na(pin_height_mm)])],
+                          cumu = pin_height_mm - first_pin_height) %>%
+            # dplyr::mutate(cumu = pin_height_mm - pin_height_mm[1]) %>% # this is Kim's formula for getting cumulative change but it doesn't work if the earliest date is NA
+            # mutate(cumu = pin_height_mm - pin_height_mm[min(which(!is.na(pin_height_mm)))]) %>% ##### subtract off the first pin reading that's not NA
+            dplyr::ungroup() %>%
 
-    # have to make sure to arrange properly so correct pin reading is subtracted off
+            # average cumulative pin change up to the arm-level
+            dplyr::group_by(network_code, park_code, site_name, station_code, SET_direction, event_date_UTC) %>%
+            dplyr::select(-pin_position) %>%
+            dplyr::summarize(mean_cumu = mean(cumu, na.rm = TRUE),
+                             sd_cumu = stats::sd(cumu, na.rm = TRUE),
+                             se_cumu = stats::sd(cumu, na.rm = TRUE)/sqrt(length(!is.na(cumu))),
+                             .groups = "drop") %>%
 
-    # by pin
-    change_cumu_pin <- data %>%
-        dplyr::arrange(network_code, park_code, site_name, station_code, SET_direction, pin_position, event_date_UTC) %>%
-        dplyr::group_by(network_code, park_code, site_name, station_code, SET_direction, pin_position) %>%
-        dplyr::mutate(first_pin_height = pin_height_mm[event_date_UTC == min(event_date_UTC[!is.na(pin_height_mm)])],
-                      cumu = pin_height_mm - first_pin_height) %>%
-        # dplyr::mutate(cumu = pin_height_mm - pin_height_mm[1]) %>% # this is Kim's formula for getting cumulative change but it doesn't work if the earliest date is NA
-        # mutate(cumu = pin_height_mm - pin_height_mm[min(which(!is.na(pin_height_mm)))]) %>% ##### subtract off the first pin reading that's not NA
-        dplyr::ungroup()
+            #average cumulative arm-level change up to the station-level
+            dplyr::group_by(network_code, park_code, site_name, station_code, event_date_UTC) %>%
+            dplyr::select(-SET_direction, mean_value = mean_cumu) %>%
+            dplyr::summarize(mean_cumu = mean(mean_value, na.rm = TRUE),
+                             sd_cumu = stats::sd(mean_value, na.rm = TRUE),
+                             se_cumu = stats::sd(mean_value, na.rm = TRUE)/sqrt(length(!is.na(mean_value))),
+                             .groups = "drop")
 
-    # pins averaged up to arms
-    change_cumu_arm <- change_cumu_pin %>%
-        dplyr::group_by(network_code, park_code, site_name, station_code, SET_direction, event_date_UTC) %>%
-        dplyr::select(-pin_position) %>%
-        dplyr::summarize(mean_cumu = mean(cumu, na.rm = TRUE),
-                         sd_cumu = stats::sd(cumu, na.rm = TRUE),
-                         se_cumu = stats::sd(cumu, na.rm = TRUE)/sqrt(length(!is.na(cumu))),
-                         .groups = "drop")
+        return(change_cumu_set_station)
 
-    # arms averaged up to SETs
-    change_cumu_station <- change_cumu_arm %>%
-        dplyr::group_by(network_code, park_code, site_name, station_code, event_date_UTC) %>%
-        dplyr::select(-SET_direction, mean_value = mean_cumu) %>%
-        dplyr::summarize(mean_cumu = mean(mean_value, na.rm = TRUE),
-                         sd_cumu = stats::sd(mean_value, na.rm = TRUE),
-                         se_cumu = stats::sd(mean_value, na.rm = TRUE)/sqrt(length(!is.na(mean_value))),
-                         .groups = "drop")
+    } else if(data_type == "MH"){
 
-    return(list(pin = change_cumu_pin, arm = change_cumu_arm, station = change_cumu_station))
-    }
+        change_cumu_mh_station <- data %>%
 
-    else if("core_measurement_depth_mm" %in% cols){
-
-        ## conditions: have correct columns in data frame
-        ## stop and give an informative message if this isn't met
-        req_clms <- c("event_date_UTC", "network_code", "park_code", "site_name", "station_code", "marker_horizon_name", "core_measurement_number", "core_measurement_depth_mm", "established_date")
-
-        if(sum(req_clms %in% names(data)) != length(req_clms)){
-            stop(paste("Your data frame must have the following columns, with these names, but is missing at least one:", paste(req_clms, collapse = ", ")))
-        }
-
-        ## calculations
-        # by marker horizon
-        change_cumu_mh <- data %>%
+            # first average all core measurements from each date
             dplyr::group_by(network_code, park_code, site_name, station_code, marker_horizon_name, event_date_UTC, established_date) %>%
             dplyr::summarise(cumu = mean(core_measurement_depth_mm, na.rm = TRUE),
                              .groups = "drop") %>%
-            dplyr::ungroup()
+            dplyr::ungroup() %>%
 
-        # marker horizons averaged up to stations
-        change_cumu_mh_station <- change_cumu_mh %>%
+            # average the core measurements from each plot up to the station-level
             dplyr::group_by(network_code, park_code, site_name, station_code, event_date_UTC, established_date) %>%
             dplyr::select(-marker_horizon_name) %>%
             dplyr::summarise(mean_cumu = mean(cumu, na.rm = TRUE),
                              sd_cumu = stats::sd(cumu, na.rm = TRUE),
                              se_cumu = stats::sd(cumu, na.rm = TRUE)/sqrt(length(!is.na(cumu))),
                              .groups = "drop") %>%
-            dplyr::mutate(mean_cumu = if_else(is.nan(mean_cumu), NA_real_, mean_cumu))
+            dplyr::mutate(mean_cumu = if_else(is.nan(mean_cumu), NA_real_, mean_cumu)) %>%
+            dplyr::ungroup()
 
-        return(list(mh = change_cumu_mh, mh_station = change_cumu_mh_station))
+        return(change_cumu_mh_station)
     }
 }
