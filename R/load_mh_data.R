d9baa7245e69395a6edd0d546421209e012f21c1
laura-feher <lfeher@nps.gov>
2025-03-05 14:45:17 -0500

    🎨 added checks to make sure user supplied data has the correct columns

--- R/load_mh_data.R
+++ R/load_mh_data.R
@@ -1,24 +1,45 @@
 #' Load MH data from database or saved file
 #'
-#' @param park_code optional; A 4 character park code that can be used to filter results from the NPS SET database to a specific park.
+#' @param park (optional); A 4 character park code that can be used to filter
+#'   results from the NPS SET database to a specific park. If not specified,
+#'   will return data for all parks.
 #'
-#' @param network_code optional; A 4 character network code that can be used to filter results from the NPS SET database to a specific I&M network.
+#' @param network_code (optional); A 4 character network code that can be used to
+#'   filter results from the NPS SET database to a specific I&M network. If not
+#'   specified, will return data for all networks.
 #'
-#' @param file_path optional; If, instead of connecting to the NPS SET database, you want to use a saved .csv or .xlsx file, the full file path to the saved file. Must be table with one row per marker horizon reading, and the following columns, named exactly: event_date_UTC, network_code, park_code, site_name, marker_horizon_name, core_measurement_number, core_measurement_depth_mm, established_date.
+#' @param file_path (optional); If, instead of connecting to the NPS SET database,
+#'   you want to use a saved .csv or .xlsx file, the full file path to the saved
+#'   file. Must be table with one row per marker horizon reading, and the
+#'   following columns, named exactly: event_date_UTC, network_code, park_code,
+#'   site_name, marker_horizon_name, core_measurement_number,
+#'   core_measurement_depth_mm, established_date.
 #'
-#' @param db_version optional; If getting data from database, do you want to connect to the production or development version of the database? Default is "production".
+#' @param db_version (optional); If getting data from database, do you want to
+#'   connect to the production or development version of the database? Default
+#'   is "production".
 #'
 #' @return A data frame of raw MH data.
 #'
+#' @details Note that you must be on VPN and have read access to the NPS I&M SET
+#'   database for the database option to work.#'
+#'
 #' @export
+#'
+#' @import DBI
+#' @import odbc
+#' @import readr
+#' @import readxl
 #'
 #' @examples
 #'
-#' load_mh_data(data_type = "database")
+#' load_mh_data(park = "ASIS")
 #'
-#' load_mh_data(data_type = "saved_file", file_path = "C:/Documents/Data/my_MH_data.csv")
+#' load_mh_data(network_code = "NCBN",  db_version = "development")
 #'
-load_mh_data <- function(park_code = NULL, network_code = NULL, file_path = NULL, db_version = "production"){
+#' load_mh_data(file_path = "C:/Documents/Data/my_MH_data.csv")
+#'
+load_mh_data <- function(park = NULL, network_code = NULL, file_path = NULL, db_version = "production"){
 
     if(is.null(file_path)) {
 
@@ -43,15 +64,15 @@ load_mh_data <- function(park_code = NULL, network_code = NULL, file_path = NULL
                               Port = 1433)
 
         # get data from SET db and filter to a specific park
-        if(!is.null(park_code) & is.null(network_code)) {
+        if(!is.null(park) & is.null(network_code)) {
             dataf <- DBI::dbSendQuery(con, 'SELECT * FROM ssrs.vw_dbx_marker_horizon_data WHERE park_code = ?')
-            data <- DBI::dbBind(dataf, list(toupper(park_code)))
+            data <- DBI::dbBind(dataf, list(toupper(park)))
             data <- DBI::dbFetch(data)
             DBI::dbClearResult(dataf)
         }
 
         # get data from SET db and filter to a specific I&M network
-        else if(!is.null(network_code) & is.null(park_code)) {
+        else if(!is.null(network_code) & is.null(park)) {
             dataf <- DBI::dbSendQuery(con, 'SELECT * FROM ssrs.vw_dbx_marker_horizon_data WHERE network_code = ?')
             data <- DBI::dbBind(dataf, list(toupper(network_code)))
             data <- DBI::dbFetch(data)
@@ -59,9 +80,9 @@ load_mh_data <- function(park_code = NULL, network_code = NULL, file_path = NULL
         }
 
         # if both park and network are specified, ignore network_code and filter to park_code
-        else if(!is.null(park_code) & !is.null(network_code)) {
+        else if(!is.null(park) & !is.null(network_code)) {
             dataf <- DBI::dbSendQuery(con, 'SELECT * FROM ssrs.vw_dbx_marker_horizon_data WHERE park_code = ?')
-            data <- DBI::dbBind(dataf, list(toupper(park_code)))
+            data <- DBI::dbBind(dataf, list(toupper(park)))
             data <- DBI::dbFetch(data)
             DBI::dbClearResult(dataf)
         }
@@ -77,11 +98,31 @@ load_mh_data <- function(park_code = NULL, network_code = NULL, file_path = NULL
 
     else if(!is.null(file_path) & stringr::str_detect(file_path, ".csv")) {
         data <- readr::read_csv(file_path)
+
+        cols <- colnames(data)
+
+        ## conditions: have correct columns in data frame
+        ## stop and give an informative message if this isn't met
+        req_clms <- c("event_date_UTC", "network_code", "park_code", "site_name", "station_code", "marker_horizon_name", "core_measurement_number", "core_measurement_depth_mm", "established_date")
+
+        if(sum(req_clms %in% names(data)) != length(req_clms)){
+            stop(paste("Your data frame must have the following columns, with these names, but is missing at least one:", paste(req_clms, collapse = ", ")))
+        }
     }
 
     else if(!is.null(file_path) & (stringr::str_detect(file_path, ".xls") | stringr::str_detect(file_path, ".xlsx"))) {
         data <- readxl::read_excel(file_path)
-    }
-    return(data)
 
+        cols <- colnames(data)
+
+        ## conditions: have correct columns in data frame
+        ## stop and give an informative message if this isn't met
+        req_clms <- c("event_date_UTC", "network_code", "park_code", "site_name", "station_code", "marker_horizon_name", "core_measurement_number", "core_measurement_depth_mm", "established_date")
+
+        if(sum(req_clms %in% names(data)) != length(req_clms)){
+            stop(paste("Your data frame must have the following columns, with these names, but is missing at least one:", paste(req_clms, collapse = ", ")))
+        }
+    }
+
+    return(data)
 }
