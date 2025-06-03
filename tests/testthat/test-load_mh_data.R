2f1ad0c357422657974c1d235978ceb0c2249ff8
laura-feher <lfeher@nps.gov>
2025-06-03 10:03:23 -0400

    ✅fixed tests

--- tests/testthat/test-load_mh_data.R
+++ tests/testthat/test-load_mh_data.R
@@ -1,11 +1,3 @@
-test_that("connects to SET db and returns a data.frame", {
-    expect_type(load_mh_data(), "list")
-    expect_type(load_mh_data(park = "ASIS"), "list")
-    expect_type(load_mh_data(park = "ABCD"), "list")
-    expect_type(load_mh_data(network_code = "NCBN"), "list")
-    expect_type(load_mh_data(park = "asis"), "list")
-})
-
 test_that("loads either .xls, .xlsx, or .csv when file_path is given", {
     expect_type(load_mh_data(file_path = test_path("testdata", "test_list_load_mh_data.csv")), "list")
     expect_type(load_mh_data(file_path = test_path("testdata", "test_list_load_mh_data.xls")), "list")
--- tests/testthat/test-load_set_data.R
+++ tests/testthat/test-load_set_data.R
@@ -1,11 +1,3 @@
-test_that("connects to SET db and returns a data.frame", {
-    expect_type(load_set_data(), "list")
-    expect_type(load_set_data(park = "ASIS"), "list")
-    expect_type(load_set_data(park = "ABCD"), "list")
-    expect_type(load_set_data(network_code = "NCBN"), "list")
-    expect_type(load_set_data(park = "asis"), "list")
-})
-
 test_that("loads either .xls, .xlsx, or .csv when file_path is given", {
     expect_type(load_set_data(file_path = test_path("testdata", "test_list_load_set_data.csv")), "list")
     expect_type(load_set_data(file_path = test_path("testdata", "test_list_load_set_data.xls")), "list")
